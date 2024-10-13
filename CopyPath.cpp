e80c610e36937c3130c5042b142c563a3ec4eb73
Adijnnuuy <96059201+Adi-SOUL@users.noreply.github.com>
2024-10-13 22:44:42 +0800

    upload files

---
+++ CopyPath.cpp
@@ -0,0 +1,65 @@
+﻿#include <string>
+#include <iostream>
+#include <sstream>
+#include <filesystem>
+#include <fstream>
+#include <windows.h>
+
+namespace fs = std::filesystem;
+using namespace std;
+
+void SetClipboardText(const std::string& text) {
+    if (OpenClipboard(nullptr)) {
+        EmptyClipboard();
+
+        HGLOBAL hGlob = GlobalAlloc(GMEM_MOVEABLE, text.size() + 1);
+        if (hGlob) {
+            char* pBuf = static_cast<char*>(GlobalLock(hGlob));
+            if (pBuf) {
+                strcpy_s(pBuf, text.size() + 1, text.c_str());
+                GlobalUnlock(hGlob);
+
+                SetClipboardData(CF_TEXT, hGlob);
+            }
+        }
+        CloseClipboard();
+    }
+}
+
+string join(vector<string>& items, const string separator) {
+    std::ostringstream oss;
+    auto it = items.begin();
+    if (it != items.end()) {
+        oss << *it;
+        ++it;
+    }
+    for (; it != items.end(); ++it) {
+        oss << separator << *it;
+    }
+    return oss.str();
+}
+
+int main(int argc, const char* argv[]) {
+    if (argc != 3) {
+        return 0;
+    }
+
+    string mode = argv[2];
+    string path = argv[1];
+    vector<string> files;
+
+    if (mode == "r") {
+        for (const auto& entry : fs::directory_iterator(fs::path(path.c_str()))) {
+            files.emplace_back(entry.path().lexically_relative(fs::path(path.c_str())).string());
+        }
+    }
+    else {
+        for (const auto& entry : fs::directory_iterator(fs::path(path.c_str()))) {
+            files.emplace_back(entry.path().string());
+        }
+    }
+
+    auto result = join(files, ";\n");
+    SetClipboardText(result);
+    return 0;
+}
---
+++ readme.md
@@ -0,0 +1,14 @@
+# CopyPath: A program that helps you copy the relative or absolute paths of multiple files in a directory
+
+## What is it?
+For some reason, we may need to paste the paths of multiple files in a directory. For example, imagine you are adding some `lib` files into your Visual Studio project (No `link_libraries` in it, haha). Boring, right? Now you can copy and paste those paths in just a few clicks with CopyPath.
+
+## How to use CopyPath?
+Right click a directory (or the background of a directory), and then choose `Copy All Relavite Paths` or `Copy All Absolute Paths`. That's all.
+
+## How to install CopyPath?
+1. Download the released files [here]();
+2. Run `install.bat` as an administrator.
+
+Alternatively, you can compile it from the source code.
+
